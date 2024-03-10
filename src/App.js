bebf71933996589ade998fd603fc7ddbb5658527
Oğuzhan Garip <oguzhan.garip21@gmail.com>
2024-03-10 18:05:21 +0300

    Örnek kodlar yazıldı

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { useEffect, useState } from "react";
 
 
 function App() {
+  const [sayac,sayacGuncelle] = useState(0)
+
+  useEffect( ()=> { 
+    console.log("1.effect calisti");
+  }, [sayac] )
+
+  useEffect( ()=> { 
+    console.log("2.effect calisti");
+  } )
+
+ 
   return (
     <>
-      App
+      Sayac: {sayac}
     </>
   
   );
